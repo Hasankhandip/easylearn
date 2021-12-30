839ec63c59b72fc6c02d2b9fefba76ea9cb2707c
Dip <hasankhandip@gmail.com>
2021-12-30 23:29:57 +0600

    blog added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,35 @@ $(window).on("scroll", function(){
     }
   });
 // header-section js end
+
+// cousre-load js start
+$(document).ready(function(){
+  $(".course-content").slice(0, 6).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".course-content:hidden").slice(0, 3).slideDown();
+    if($(".course-content:hidden").length == 0) {
+      $("#loadMore").addClass("noContent");
+    }
+  });
+  
+})
+// cousre-load js end
+
+// odometer js start
+$(".count").counterUp({
+  delay: 10,
+  time: 1000,
+});
+// odometer js end
+
+ // slick slider start
+ $(document).ready(function(){
+  $('.testimonial-area').slick({
+    prevArrow: ".left-arrow-testimonial",
+    nextArrow:  ".right-arrow-testimonial",
+    slidesToShow: 1,
+  slidesToScroll: 1
+  });
+});
+// slick slider end
